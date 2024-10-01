88b0e16145dc5ee8ce3b4101b4fee02fa6c34475
Alvin Berglund <albe0411@storsjogymnasiet.se>
2024-10-01 11:26:08 +0200

    rock paper scissors

--- main.py
+++ main.py
@@ -143,10 +143,57 @@ def magic_8ball():
     print("the answer is: " + answer)
     print()
 
+def rock_paper_scissors():
+
+    computer_score = 0
+    user_score = 0
     
+    while user_score <= 2 and computer_score <= 2:
+        user_choise = input("pick rock, paper or scissors (write break to end): ").lower()
+        print()
+        random_pick = random.choice(["rock", "paper", "scissors"])
+
+        if user_choise == "break":
+            break
+        elif user_choise != "rock" and user_choise != "paper" and user_choise != "scissors":
+            print("invalid")
+        elif random_pick == user_choise:
+            print("tie")
+            print("user score: " + str(user_score))
+            print("computer score: " + str(computer_score))
+            print()
+        elif random_pick == "rock" and user_choise == "paper":
+            print("user won")
+            user_score += 1
+            print("user score: " + str(user_score))
+            print("computer score: " + str(computer_score))
+            print()
+        elif random_pick == "paper" and user_choise == "scissors":
+            print("user won")
+            user_score += 1
+            print("user score: " + str(user_score))
+            print("computer score: " + str(computer_score))
+            print()
+        elif random_pick == "scissors" and user_choise == "rock":
+            print("user won")
+            user_score += 1
+            print("user score: " + str(user_score))
+            print("computer score: " + str(computer_score))
+            print()
+        else:
+            print("computer won")
+            computer_score += 1
+            print("user score: " + str(user_score))
+            print("computer score: " + str(computer_score))
+            print()
+        
+    if user_score == 3:
+        print("user won big")
+    elif computer_score == 3:
+        print("computer won big")
 
 print()
-question = input("pick a program (v, pr, n, m, pa, r, oen, rl, c, mb): ").lower()
+question = input("pick a program (v, pr, n, m, pa, r, oen, rl, c, mb, rps): ").lower()
 
 if question == "v":
     vowels()
@@ -168,5 +215,7 @@ elif question == "c":
     calculator()
 elif question == "mb":
     magic_8ball()
+elif question == "rps":
+    rock_paper_scissors()
 else:
-    print("unvalid input")
+    print("invalid input")
