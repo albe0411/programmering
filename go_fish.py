b6b495c7f64f8a2d8e86060b306c3045a6fb59b2
Alvin Berglund <albe0411@storsjogymnasiet.se>
2024-12-03 09:54:30 +0100

    go fish 3/3

--- go_fish.py
+++ go_fish.py
@@ -1,53 +1,103 @@
 import random
 
-#add func
 deck = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k',
         'a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k',
         'a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k',
         'a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k']
 
-player1 = []
-player2 = []
-player3 = []
+player1_hand = []
+player2_hand = []
+player3_hand = []
 
 player1_point = 0
 player2_point = 0
 player3_point = 0
 
-#add func
+player1_player_choice = ["2", "3"]
+player2_player_choice = ["1", "3"]
+player3_player_choice = ["1", "2"]
+
+player_turn = random.randint(1, 3)
+
 random.shuffle(deck)
 
 for i in range(7):
-    player1.append(deck[0])
+    player1_hand.append(deck[0])
     deck.pop(0)
-    player2.append(deck[0])
+    player2_hand.append(deck[0])
     deck.pop(0)
-    player3.append(deck[0])
+    player3_hand.append(deck[0])
     deck.pop(0)
 
-print(player1)
-print(player2)
-print(player3)
-
-print(deck)
-
-#add func
 while True:
-    #add loop player turn (random order)
-    #add if for player or computer
-    #if computer > randomize
+    print()
+    print("player 1 score: ", player1_point)
+    print("player 2 score: ", player2_point)
+    print("player 3 score: ", player3_point)
+    print()
 
-    question_player = input("2, 3: ")
-    question_card = input("a, 2, 3, 4, 5, 6, 7, 8, 9, 10, j, q, k: ") #add try/catch
+    print("player's turn: ", player_turn)
 
+    question_player = 0
+    question_card = 0
 
-    if eval(f"question_card in player{question_player}"):
-        while eval(f"question_card in player{question_player}"):
-            exec(f"player{question_player}.remove(question_card)")
-            player1.append(question_card) #add exac
+    if eval(f"player{player_turn}_hand"):
+        if player_turn == 1:
+            while True:
+                print(player1_hand)
+                question_player = input(str(player1_player_choice) + ": ")
+                question_card = input(str(player1_hand) + ": ").lower()
+
+                if question_player in player1_player_choice and question_card in player1_hand:
+                    break
+                else:
+                    print("invalid input")
+        else:
+            exec(f"question_player = random.choice(player{player_turn}_player_choice)")
+            exec(f"question_card = random.choice(player{player_turn}_hand)")
+
+    if question_player == 0 and question_card == 0:
+        print("players hand is empty")
     else:
-        player1.append(deck[0]) #add exac
+        print("player asked: ", question_player)
+        print("card asked about: ", question_card)
 
-    print(player3)
-    print(player2)
-    print(player1)
+    if question_player != 0 and question_card in eval(f"player{question_player}_hand"):
+        amount_of_cards_taken = 0
+        while eval(f"question_card in player{question_player}_hand"):
+            exec(f"player{question_player}_hand.remove(question_card)")
+            exec(f"player{player_turn}_hand.append(question_card)")
+            amount_of_cards_taken += 1
+        print("amount of cards taken: ", amount_of_cards_taken)
+    elif len(deck) != 0:
+        exec(f"player{player_turn}_hand.append(deck[0])")
+        deck.pop(0)
+        print("a card was taken from the deck")
+    else:
+        print("the deck is empty")
+
+    cards_in_hand = {}
+
+    for i in eval(f"range(len(player{player_turn}_hand))"):
+        if eval(f"player{player_turn}_hand[i] in cards_in_hand"):
+            exec(f"cards_in_hand[player{player_turn}_hand[i]] += 1")
+        else:
+            exec(f"cards_in_hand[player{player_turn}_hand[i]] = 1")
+
+    for i in cards_in_hand:
+        if cards_in_hand[i] == 4:
+            for x in range(4):
+                exec(f"player{player_turn}_hand.remove(str(i))")
+            exec(f"player{player_turn}_point += 1")
+
+    if not deck and not player1_hand and not player2_hand and not player3_hand:
+        print()
+        print("player 1 score: ", player1_point)
+        print("player 2 score: ", player2_point)
+        print("player 3 score: ", player3_point)
+        print()
+        break
+
+    player_turn += 1
+    if player_turn == 4:
+        player_turn = 1
